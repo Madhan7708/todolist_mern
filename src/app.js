775d2c2a01e14d734909f331ae173736e2cf1a17
Madhan7708 <bashamadhan002@gmail.com>
2025-04-13 15:48:35 +0530

    Initial commit -2

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express=require('express');
 const app=express();
+app.use(express.json());
+const mongoose=require('mongoose');
 const todoRouter=require('./routes/todo');
 app.use('/todo',todoRouter);
 
 
-app.listen(3000,()=>{
-    console.log("server is running at 3000");
+mongoose.connect('mongodb://localhost:27017/crud').then(()=>{
+    app.listen(3000,()=>{
+        console.log("server is running at 3000 and db connected");
+    })
+})
+.catch((err)=>{
+    console.log(err);
 })
--- src/routes/todo.js
+++ src/routes/todo.js
@@ -1,7 +1,18 @@
 const express=require('express');
+const mongoose=require('mongoose');
+const usermodel=require('../models/todolist');
 const router=express.Router();
 
 router.get('/home',(req,res)=>{
-    res.send("send request to clients");
+    res.send({message:"Welcome to the Todo API"});
 })
+
+
+router.post('/', async(req,res)=>{
+    const {title,description}=req.body;
+    const todo=await usermodel({title,description});
+    res.status(200).send(todo);
+})
+
+
 module.exports=router;
