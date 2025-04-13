1ff6fd99aaf321ff16180d871e0a3c09b06c8143
Madhan7708 <bashamadhan002@gmail.com>
2025-04-13 16:42:24 +0530

    Initial commit :3 delete  operations done

--- src/routes/todo.js
+++ src/routes/todo.js
@@ -7,8 +7,6 @@ router.get('/home',(req,res)=>{
     res.send({message:"Welcome to the Todo API"});
 })
 
-
-
 router.post('/', async(req,res)=>{
     const {title,description}=req.body;
     const todo=await usermodel.create({title,description});
@@ -23,6 +21,12 @@ router.get('/get',async(req,res)=>{
 router.get('/get/:id',async(req,res)=>{
     const id=req.params.id;
     const todo =await usermodel.findOne({_id:id});
+    res.status(200).json(todo);
+})
+
+router.delete('/delete/:id',async(req,res)=>{
+    const id=req.params.id;
+    const todo=await usermodel.deleteOne({_id:id});
     res.status(200).json(todo);
 })
 
